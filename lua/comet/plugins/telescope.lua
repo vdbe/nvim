e10fe4ce66875842ed7f645f6e46c5cabbaf4c0c
vdbewout <vdbewout@gmail.com>
2024-05-28 18:58:27 +0000

    fix(comet): enable fzf-native on nix

--- lua/comet/plugins/telescope.lua
+++ lua/comet/plugins/telescope.lua
@@ -3,6 +3,12 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     optional = true,
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        enabled = vim.g.is_nix or (vim.fn.executable "make" == 1),
+      },
+    },
     opts = function()
       local actions = require "telescope.actions"
 
