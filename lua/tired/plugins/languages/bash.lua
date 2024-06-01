a9c6b07254d0c9d4de532acea2830f152e2b3771
vdbewout <vdbewout@gmail.com>
2024-06-01 20:24:59 +0000

    feat(tired): lsp bash

---
+++ lua/tired/plugins/languages/bash.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {
+          filetypes = { "bash", "tmux" },
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "bash" })
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "bashls" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "bash-language-server", "shfmt", "shellcheck" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "bash" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        sh = { "shfmt" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        sh = { "shellcheck" },
+      },
+    },
+  },
+}
--- lua/tired/plugins/languages/init.lua
+++ lua/tired/plugins/languages/init.lua
@@ -1,5 +1,7 @@
 return {
   { import = "lazyvim.plugins.extras.lang.rust" },
+
+  { import = "tired.plugins.languages.bash" },
   { import = "tired.plugins.languages.lua" },
   { import = "tired.plugins.languages.nix" },
 }
