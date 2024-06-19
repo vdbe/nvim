9ae671cfa6b642d2325e56a5a24fbdb200aac68e
vdbewout <vdbewout@gmail.com>
2024-06-19 18:39:31 +0000

    add script to update lazy lockfile

--- nix/apps.nix
+++ nix/apps.nix
@@ -19,6 +19,30 @@ let
   update-plugins = mkUpdateSource "update-plugins" "${sources}/plugins";
   update-grammars = mkUpdateSource "update-grammars" "${sources}/grammars";
 
+  update-lazy-lock = pkgs.writeShellApplication {
+    name = "update-lazy-lock";
+    runtimeInputs = with pkgs; [
+      neovim
+      gitMinimal
+    ];
+    text = ''
+      declare tmpdir
+      tmpdir="$(mktemp -d)"
+
+      # Setup nvim config
+      mkdir -p "$tmpdir/.config"
+      ln -s "$PWD" "$tmpdir/.config/nvim"
+
+      # shellcheck disable=SC2046
+      unset $(env | grep "^XDG_" | cut -d= -f1)
+
+      HOME="$tmpdir" nvim --headless '+Lazy! update' '+qa!' 2>/dev/null
+
+      unlink "$tmpdir/.config/nvim"
+      rm -rf "$tmpdir"
+    '';
+  };
+
   update-sources = pkgs.writeShellApplication {
     name = "update-sources";
     runtimeInputs = [
@@ -32,7 +56,25 @@ let
     '';
   };
 
+  update-all = pkgs.writeShellApplication {
+    name = "update-sources";
+    runtimeInputs = [
+      update-sources
+      update-lazy-lock
+    ];
+    text = ''
+      set -e
+      update-sources
+      update-lazy-lock
+    '';
+  };
+
   apps = {
+    update-all = {
+      type = "app";
+      program = lib.getExe update-all;
+    };
+
     update-sources = {
       type = "app";
       program = lib.getExe update-sources;
@@ -44,6 +86,11 @@ let
     update-grammars = {
       type = "app";
       program = lib.getExe update-grammars;
+    };
+
+    update-lazy-lock = {
+      type = "app";
+      program = lib.getExe update-lazy-lock;
     };
   };
 in
